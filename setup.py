d85d4edcdf2c70d9f9fd3b76d8baf484c379a30d
yumiguan <yumiguan1009@gmail.com>
2019-06-04 17:46:46 +0800

    add ideviceinstaller

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-ios',
-    version='0.2.6',
+    version='0.2.7',
     packages=['lyrebird_ios'],
     url='https://github.com/meituan/lyrebird-ios',
     author='HBQA',
